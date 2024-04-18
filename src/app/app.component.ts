941772a0d610790ee06a1ccc2b919effbfd8dd59
Anton Kirschhock <anton.kirschhock@skillerjobs.com>
2024-04-18 08:08:38 +0200

    Reworked to latest angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { RouterOutlet } from '@angular/router';
-
+import { TranslationNode } from './helpers';
+import { Step1Component } from './step1/step1.component';
+import { Step2Component } from './step2/step2.component';
+import { Step3Component } from './step3/step3.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    Step1Component,
+    Step2Component,
+    Step3Component,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'i18neditor';
+  step = 1;
+  nodes: TranslationNode[] = [];
+
+  next() {
+    if (this.step === 1 && (!this.nodes || this.nodes.length === 0)) {
+      return;
+    }
+
+    if (this.step < 3) {
+      this.step++;
+    }
+  }
+
+  prev() {
+    if (this.step > 1) {
+      this.step--;
+    }
+  }
 }
